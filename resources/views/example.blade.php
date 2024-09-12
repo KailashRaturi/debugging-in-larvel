4123017bb683f150ca95868f7ae605bc4ebe5151
unknown <kailashraturi@8gmail.com>
2024-09-12 13:31:27 +0530

    Initial commit

---
+++ resources/views/example.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/example.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example View</title>
+</head>
+<body>
+    <h1>This is the Example View!</h1>
+    <p>Welcome to the example page.</p>
+</body>
+</html>
